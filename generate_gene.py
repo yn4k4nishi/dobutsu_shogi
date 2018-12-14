7a08bffe1a1c45b9ea0a651f7e015ad12490f7e3
yusuke nakanishi <you@example.com>
2018-12-14 14:55:38 +0900

    create kkp.csv

--- generate_gene.py
+++ generate_gene.py
@@ -1,2 +1,13 @@
 #coding:utf-8
+import numpy as np
 import random
+import csv
+
+kkp_first = np.random.randint(100, size=(12,12*11*5))
+
+print(kkp_first)
+
+with open('kkp.csv', 'w') as f:
+    writer = csv.writer(f, lineterminator='\n') # 改行コード（\n）を指定しておく
+    # writer.writerow(list)     # list（1次元配列）の場合
+    writer.writerows(kkp_first) # 2次元配列も書き込める

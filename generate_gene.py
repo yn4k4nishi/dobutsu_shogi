5b467771b3796652737a163432bb123ca61a1034
yusuke nakanishi <you@example.com>
2018-12-14 17:49:06 +0900

    Update csv file

--- generate_gene.py
+++ generate_gene.py
@@ -3,11 +3,24 @@ import numpy as np
 import random
 import csv
 
-kkp_first = np.random.randint(100, size=(12,12*11*5))
+def generate_gene(csvfile):
+    ### 遺伝子の作成
+    kkp_first = np.random.randint(100, size=(12*11*5,12-2))
+    # kkp_first[0][0] = None
+    # print(kkp_first)
 
-print(kkp_first)
+    ### CSVファイルに書き込み
+    with open(csvfile, 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(kkp_first)
+    print("generate gene : "+ csvfile)
 
-with open('kkp.csv', 'w') as f:
-    writer = csv.writer(f, lineterminator='\n') # 改行コード（\n）を指定しておく
-    # writer.writerow(list)     # list（1次元配列）の場合
-    writer.writerows(kkp_first) # 2次元配列も書き込める
+#############################################
+#####               main                #####
+#############################################
+generate_gene('kkp_gene/gene_0.csv')
+generate_gene('kkp_gene/gene_1.csv')
+generate_gene('kkp_gene/gene_2.csv')
+generate_gene('kkp_gene/gene_3.csv')
+generate_gene('kkp_gene/gene_4.csv')
+generate_gene('kkp_gene/gene_5.csv')
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #coding:utf-8
 import csv
-import generate_gene.py
+
+def read_gene():
+    with open('kkp_gene/gene_0.csv', 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)  # ヘッダーを読み飛ばしたい時
+
+        for row in reader:
+            print(row)          # 1行づつ取得できる

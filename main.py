dcaef3dbc838e855418a6b21718861a09b4cd14d
yusuke nakanishi <you@example.com>
2018-12-14 18:19:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 #coding:utf-8
 import csv
+import pandas as pd
 
-def read_gene():
-    with open('kkp_gene/gene_0.csv', 'r') as f:
-        reader = csv.reader(f)
-        header = next(reader)  # ヘッダーを読み飛ばしたい時
+#   盤面
+# +--------+
+# | 1| 2| 3|
+# | 4| 5| 6|
+# | 7| 8| 9|
+# |10|11|12|
+# +--------+
 
-        for row in reader:
-            print(row)          # 1行づつ取得できる
+# Aのライオン :  1
+# Aのゾウ     :  2
+# Aのキリン   :  3
+# Aのニワトリ :  4
+# Aのヒヨコ   :  5
+# Bのライオン : -1
+# Bのゾウ     : -2
+# Bのキリン   : -3
+# Bのニワトリ : -4
+# Bのヒヨコ   : -5
+# なにもないところは 0
+boad = [-3,-1,-2,0,-5,0,]
+
+geneA = pd.read_csv("kkp_gene/gene_0.csv").values.tolist()
+geneB = pd.read_csv("kkp_gene/gene_1.csv").values.tolist()
+# print(geneA)
+
+def checkmate():
